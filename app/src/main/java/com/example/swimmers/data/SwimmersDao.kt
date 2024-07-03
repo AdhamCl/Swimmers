252bba94d8b3f4546433a69efef96e09d40a3b73
AdhamCl <AdhamCl@outlook.com>
2024-07-03 22:19:30 +0100

    commit

--- app/src/main/java/com/example/swimmers/data/SwimmersDao.kt
+++ app/src/main/java/com/example/swimmers/data/SwimmersDao.kt
@@ -1,5 +1,7 @@
 package com.example.swimmers.data
 
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
@@ -8,5 +10,11 @@ interface SwimmersDao {
     @Query("SELECT * FROM members_table ORDER BY id ASC")
     fun getAllMembers(): Flow<List<Members>>
 
+
+    @Query("SELECT * FROM members_table WHERE isPay = 1 ORDER BY id ASC")
+    fun getPayMembers(): Flow<List<Members>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addNewMember(members: Members)
 
 }
