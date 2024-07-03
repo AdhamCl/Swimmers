daf445a048d6d0d382eec223e717dcd6baedb235
AdhamCl <AdhamCl@outlook.com>
2024-07-03 22:47:41 +0100

    commit

---
+++ app/src/main/java/com/example/swimmers/di/Module.kt
@@ -0,0 +1,34 @@
+package com.example.swimmers.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.swimmers.Constants.Swimmers_DataBAse
+import com.example.swimmers.data.SwimmersDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object Module {
+
+    @Provides
+    @Singleton
+    fun provideTasksDatabase(
+        @ApplicationContext context: Context
+    )
+            = Room.databaseBuilder(
+        context,
+        SwimmersDatabase::class.java,
+        Swimmers_DataBAse
+    ).build()
+
+    @Provides
+    @Singleton
+    fun provideSwillersDao(database: SwimmersDatabase) = database.swimmersDao()
+
+
+}
